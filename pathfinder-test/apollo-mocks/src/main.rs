ba7ccf7886ace780bf01008277a15533a4856028
Krisztian Kovacs <kkovaacs@gmail.com>
2025-11-26 11:01:45 +0100

    pathfinder-mocks: add fake feeder gateway endpoints
    Not having these would block Pathfinder from starting up.

--- pathfinder-test/apollo-mocks/src/main.rs
+++ pathfinder-test/apollo-mocks/src/main.rs
@@ -1,8 +1,6 @@
-use axum::{
-    Json, Router,
-    extract::Query,
-    routing::{get, post},
-};
+use axum::extract::Query;
+use axum::routing::{get, post};
+use axum::{Json, Router};
 use tower_http::trace::TraceLayer;
 use tracing_subscriber::EnvFilter;
 
@@ -21,6 +19,19 @@ async fn eth_to_strk_oracle_get_price(
     }))
 }
 
+async fn feeder_gateway_get_contract_addresses() -> Json<serde_json::Value> {
+    Json(serde_json::json!({
+        "Starknet": "0x5fbdb2315678afecb367f032d93f642f64180aa3",
+        "GpsStatementVerifier": "0x47312450B3Ac8b5b8e247a6bB6d523e7605bDb60",
+        "strk_l2_token_address": "0x4718f5a0fc34cc1af16a1cdee98ffb20c31f5cd61d6ab07201858f4287c938d",
+        "eth_l2_token_address": "0x49d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7"
+    }))
+}
+
+async fn feeder_gateway_get_public_key() -> Json<serde_json::Value> {
+    Json(serde_json::json!("0x48253ff2c3bed7af18bde0b611b083b39445959102d4947c51c4db6aa4f4e58"))
+}
+
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     let format = tracing_subscriber::fmt::format().compact();
@@ -29,19 +40,16 @@ async fn main() -> std::io::Result<()> {
         .with_env_filter(EnvFilter::from_default_env())
         .init();
 
-    let listener_address = std::env::args()
-        .nth(1)
-        .unwrap_or_else(|| "[::]:8080".to_owned());
+    let listener_address = std::env::args().nth(1).unwrap_or_else(|| "[::]:8080".to_owned());
     tracing::info!(%listener_address, "Starting up");
     let listener = tokio::net::TcpListener::bind(listener_address).await?;
 
     let router = Router::new()
         .route("/cende_recorder/write_blob", post(move || async { "" }))
-        .route(
-            "/cende_recorder/write_pre_confirmed_block",
-            post(move || async { "" }),
-        )
-        .route("/eth_to_strk_oracle", get(eth_to_strk_oracle_get_price));
+        .route("/cende_recorder/write_pre_confirmed_block", post(move || async { "" }))
+        .route("/eth_to_strk_oracle", get(eth_to_strk_oracle_get_price))
+        .route("/feeder_gateway/get_contract_addresses", get(feeder_gateway_get_contract_addresses))
+        .route("/feeder_gateway/get_public_key", get(feeder_gateway_get_public_key));
 
     axum::serve(listener, router.layer(TraceLayer::new_for_http())).await
 }
